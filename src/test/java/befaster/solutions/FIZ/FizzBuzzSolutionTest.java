36d79e41e2b0be8367a7b21874d8dad442d390c1
sbx_user1060 <sbx_user1060@ip-10-128-250-122.eu-west-2.compute.internal>
2018-09-02 17:32:09 +0000

    Sun Sep 02 17:32:09 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,4 +1,30 @@
 package befaster.solutions.FIZ;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzSolutionTest {
+
+    private FizzBuzzSolution fizz;
+
+    @Before
+    public void setUp() {
+
+        fizz = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void check_fizz_buzz() {
+        assertThat(fizz.fizzBuzz(1), is(1));
+    }
+
+    @Test
+    public void check_fizz_buzz() {
+        assertThat(fizz.fizzBuzz(1), is(1));
+    }
+
 }
