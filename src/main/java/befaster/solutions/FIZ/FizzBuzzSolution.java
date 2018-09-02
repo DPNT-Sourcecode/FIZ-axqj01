7fa371ed432b68cc4f9cb58ad25c6584b1db01c6
sbx_user1060 <sbx_user1060@ip-10-128-250-122.eu-west-2.compute.internal>
2018-09-02 18:02:54 +0000

    Sun Sep 02 18:02:54 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,16 +2,22 @@ package befaster.solutions.FIZ;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class FizzBuzzSolution {
 
     private static final String THE_NUMBER_IS_INVALID = "The number: %s is invalid.";
     private static final String FIZZ = "fizz ";
     private static final String BUZZ = "buzz ";
+    private static final String DELUX = "delux ";
     private static final int DIVIDER_3 = 3;
     private static final int DIVIDER_5 = 5;
 
 
-    public String fizzBuzz(Integer num){
+    public String fizzBuzz(Integer num) {
 
         isValidInput(num);
 
@@ -20,16 +26,25 @@ public class FizzBuzzSolution {
         result.append(fizzBuzzResolver(num, DIVIDER_3, FIZZ));
         result.append(fizzBuzzResolver(num, DIVIDER_5, BUZZ));
 
+        if (digitsAreIdenticalInNumAboveTen(num)) {
+            result.append(DELUX);
+        }
+
         return result.length() == 0 ? String.valueOf(num) : result.toString().trim();
 
     }
 
-    private static String fizzBuzzResolver(int num, int divider, String chunk){
+    private static boolean digitsAreIdenticalInNumAboveTen(int num) {
+        final List<String> digits = Arrays.asList(String.valueOf(num).split(""));
+        return digits.size() > 1 && new HashSet<>(digits).size() == 1;
+    }
+
+    private static String fizzBuzzResolver(int num, int divider, String chunk) {
         return reminderIsEmpty(num, divider) || hasNumber(num, divider) ? chunk : "";
     }
 
     private static boolean hasNumber(int num, int targetNum) {
-        for(int i = num; i > 0 ; i /= 10){
+        for (int i = num; i > 0; i /= 10) {
             if (i % 10 == targetNum) {
                 return true;
             }
@@ -37,12 +52,12 @@ public class FizzBuzzSolution {
         return false;
     }
 
-    private static boolean reminderIsEmpty(int num, int divider){
+    private static boolean reminderIsEmpty(int num, int divider) {
         return num % divider == 0;
     }
 
     private void isValidInput(final Integer num) {
-        if(num < 1 || num > 9999){
+        if (num < 1 || num > 9999) {
             throw new IllegalArgumentException(String.format(THE_NUMBER_IS_INVALID, num));
         }
     }
